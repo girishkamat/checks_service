1a25a0904b1c428f1bb8e7d98aadfcdebc8996ce
girishkamat <girish.kamat@gmail.com>
2020-03-13 16:21:37 +0000

    added mock response capability

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser')
 const swaggerJSDoc = require('swagger-jsdoc');
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 app.use(bodyParser.json())
 // AML check router
 app.use('/', require('./routes/aml-check-route'))
